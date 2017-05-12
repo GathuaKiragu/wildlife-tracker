55bce7070835542e2f95d0f04458e097781f7176
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-12 11:41:53 +0300

    Updated app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,10 +7,16 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    get("/", (request, response) -> {
-     Map<String, Object> model = new HashMap<String, Object>();
-     model.put("template", "templates/index.vtl" );
-     return new ModelAndView(model, "templates/layout.vtl");
-   }, new VelocityTemplateEngine());
+    String layout = "templates/layout.vtl";
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+      port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+      port = 4567;
+    }
+
+    setPort(port);
+
  }
 }
