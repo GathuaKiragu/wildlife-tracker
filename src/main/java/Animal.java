8b157b1a0bf755bab7c4a6ad6dcac39e9071be36
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-12 12:59:21 +0300

    Database Setup

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,6 @@
 public class Animal {
   private String name;
+  private int id;
 
 // Constructor
   public Animal(String name) {
@@ -9,4 +10,19 @@ public class Animal {
   public String getName(){
     return name;
   }
+//method to get animal id
+  public int getId(){
+      return id;
+  }
+
+//ovarride method
+  @Override
+public boolean equals(Object otherAnimal){
+  if (!(otherAnimal instanceof Animal)) {
+    return false;
+  } else {
+    Animal newAnimal = (Animal) otherAnimal;
+    return this.getName().equals(newAnimal.getName());
+    }
+}
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -3,15 +3,32 @@ import static org.junit.Assert.*;
 import org.sql2o.*;
 
 public class AnimalTest {
+//test to check if animal instantiates correctly
   @Test
   public void animal_instantiatesCorrectly_true() {
     Animal testAnimal = new Animal("leopard");
     assertEquals(true, testAnimal instanceof Animal);
   }
 
+//test to check if animal instantiates correctly eith name
   @Test
   public void Animal_instantiatesWithName_String() {
     Animal testAnimal = new Animal("leopard");
     assertEquals("leopard", testAnimal.getName());
   }
+// Test to ovarride equals
+  @Test
+  public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+    Animal firstAnimal = new Animal("leopard");
+    Animal anotherAnimal = new Animal("leopard");
+    assertTrue(firstAnimal.equals(anotherAnimal));
+  }
+  
+//Saving Animals
+  @Test
+ public void save_returnsTrueIfDescriptionsAretheSame() {
+   Animal testAnimal = new Animal("leopard");
+   testAnimal.save();
+   assertTrue(Animal.all().get(0).equals(testAnimal));
+ }
 }
