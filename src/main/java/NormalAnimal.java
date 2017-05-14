bf3dd7d0fc473634d9de3fbe7330e1288c574a2c
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-14 16:46:45 +0300

    Added method to save find and and ovarride method Test to save and Find Normal animal passes

--- src/main/java/NormalAnimal.java
+++ src/main/java/NormalAnimal.java
@@ -5,6 +5,7 @@ import org.sql2o.*;
 public class NormalAnimal{
   private String name;
   private String endangered;
+  private int id;
 
   public static final String isEndangered = "false";
 
@@ -13,4 +14,55 @@ public class NormalAnimal{
     this.name = name;
     endangered = isEndangered;
   }
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  // Save Method
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      if (name.equals("")){
+        throw new UnsupportedOperationException("Enter name of NormalAnimal");
+      }
+      String sql = "INSERT INTO animals (name, endangered) VALUES (:name, :endangered)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("endangered", this.endangered)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<NormalAnimal> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM animals WHERE endangered='false'";
+      return con.createQuery(sql)
+      .throwOnMappingFailure(false)
+      .executeAndFetch(NormalAnimal.class);
+    }
+  }
+// Find method
+  public static NormalAnimal find(int id) {
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM animals WHERE id=:id";
+      return con.createQuery(sql)
+      .addParameter("id", id)
+      .throwOnMappingFailure(false)
+      .executeAndFetchFirst(NormalAnimal.class);
+    }
+  }
+  // Ovaride method
+  @Override
+public boolean equals(Object otherNormalAnimal){
+  if (!(otherNormalAnimal instanceof NormalAnimal)) {
+    return false;
+  } else {
+    NormalAnimal newNormalAnimal = (NormalAnimal) otherNormalAnimal;
+    return this.getName().equals(newNormalAnimal.getName());
+    }
+}
 }
--- src/test/java/NormalAnimalTest.java
+++ src/test/java/NormalAnimalTest.java
@@ -15,4 +15,26 @@ public class NormalAnimalTest {
     NormalAnimal newNormalAnimal = new NormalAnimal("Lion");
     assertEquals(true, newNormalAnimal instanceof NormalAnimal);
   }
+// Test to check if normalAnimal instantiates with name
+  @Test
+  public void getName_normalAnimalInstantiatesWithName() {
+    NormalAnimal newNormalAnimal = new NormalAnimal("Lion");
+    assertEquals("Lion", newNormalAnimal.getName());
+  }
+// Test if normalAnimal can be saved to Database
+  @Test
+  public void save_savesIntoDatabase_true() {
+    NormalAnimal newNormalAnimal = new NormalAnimal("Lion");
+    newNormalAnimal.save();
+    assertTrue(NormalAnimal.all().get(0).equals(newNormalAnimal));
+  }
+//Test To find Normal animal with given Id
+  @Test
+  public void find_returnsNormalAnimalWithSameId_secondAnimal() {
+    NormalAnimal firstNormalAnimal = new NormalAnimal("Lion");
+    firstNormalAnimal.save();
+    NormalAnimal secondNormalAnimal = new NormalAnimal("Antelope");
+    secondNormalAnimal.save();
+    assertEquals(NormalAnimal.find(secondNormalAnimal.getId()), secondNormalAnimal);
+  }
 }
